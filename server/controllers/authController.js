9a9193c805dfa1135c24b160a18ad065e040f5d9
saile <eliasiv.codes@gmail.com>
2023-11-19 17:09:45 -0800

    impl. register route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,15 @@
+import bcrypt from "bcryptjs";
 import { StatusCodes } from "http-status-codes";
 
 import userModel from "../models/userModel.js";
 
 export const register = async (req, res) => {
-  res.send();
+  const salt = await bcrypt.genSalt(10);
+  req.body.password = await bcrypt.hash(req.body.password, salt);
+
+  const registeredUser = await userModel.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ msg: "(Server message) User registered", data: registeredUser });
 };
 
 export const login = (req, res) => {};
