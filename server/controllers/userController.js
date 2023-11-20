1b9880561eaa0fe7ec83b22911ced0807c73b17a
saile <eliasiv.codes@gmail.com>
2023-11-20 15:14:24 -0800

    impl. get followers and following routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,13 +2,19 @@ import { StatusCodes } from "http-status-codes";
 import userModel from "../models/userModel.js";
 
 export const getProfileInfo = async (req, res) => {
-  console.log(req.userInfo);
+  const user = await userModel.findById(req.userInfo.userId).select("-_id -email -password -chats -__v");
 
-  const requestedUser = await userModel.findById(req.userInfo.userId).select("-_id -email -password -chats -__v");
-
-  res.status(StatusCodes.OK).json({ msg: "(Server message) Retrieved user info", data: requestedUser });
+  res.status(StatusCodes.OK).json({ msg: "(Server message) Retrieved user info", data: user });
 };
 
-export const getFollowers = async (req, res) => {};
+export const getFollowers = async (req, res) => {
+  const { followers } = await userModel.findById(req.userInfo.userId);
 
-export const getFollowing = async (req, res) => {};
+  res.status(StatusCodes.OK).json({ msg: "(Server message) Retrieved user followers", data: { followers } });
+};
+
+export const getFollowing = async (req, res) => {
+  const { following } = await userModel.findById(req.userInfo.userId);
+
+  res.status(StatusCodes.OK).json({ msg: "(Server message) Retrieved user following", data: { following } });
+};
